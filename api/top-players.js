24e4611657dde2c4f10148be25fffa5a56b94e47
meledych1111-ux <meledych1111@gmail.com>
2026-02-08 05:41:34 +0300

    Update top-players.js

--- api/top-players.js
+++ api/top-players.js
@@ -1,6 +1,6 @@
-const { getTopPlayers } = require('./db.js');
+import { getTopPlayers } from './db.js';
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -10,7 +10,7 @@ module.exports = async function handler(req, res) {
     
     const topPlayers = await getTopPlayers(gameType, parseInt(limit));
     
-    return res.status(200).json(topPlayers);
+    return res.status(200).json(topPlayers || []);
     
   } catch (error) {
     console.error('Error getting top players:', error);
@@ -18,4 +18,4 @@ module.exports = async function handler(req, res) {
       error: 'Internal server error'
     });
   }
-};
+}
